bc31ed27eb626de44ae20708c6c3c56656c77108
y.nak <y.nak1930@gmail.com>
2020-05-01 05:41:19 +0000

    fix:migrations defaultvalue

--- database/migrations/2020_04_30_225813_add_category_id_to_tasks_table.php
+++ database/migrations/2020_04_30_225813_add_category_id_to_tasks_table.php
@@ -14,7 +14,7 @@ class AddCategoryIdToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->integer('category_id');
+            $table->integer('category_id')->default(0);
         });
     }
 
